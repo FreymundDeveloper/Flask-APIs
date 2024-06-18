2b4d16976d677bf991d72d5d84387e4d9c7b44bf
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-06-18 15:23:39 -0300

    Login System

--- Basic_Hotel_API/resources/hotel.py
+++ Basic_Hotel_API/resources/hotel.py
@@ -1,5 +1,6 @@
 from flask_restful import Resource, reqparse
 from models.hotel import HotelModel
+from flask_jwt_extended import jwt_required
 
 class Hotels(Resource):
     ## API Route Methods
@@ -20,6 +21,7 @@ class Hotel(Resource):
             if hotel: return hotel.json()
             return {'message': 'Hotel not found!'}, 404
 
+    @jwt_required()
     def post(self, hotel_id):
         if HotelModel.find_hotel(hotel_id): return { 'messege': 'Hotel id ({}) already exists!'.format(hotel_id) }, 400
 
@@ -32,6 +34,7 @@ class Hotel(Resource):
         except:
             return { 'message': 'An internal error ocurred trying to save data.'}, 500
 
+    @jwt_required()
     def put(self, hotel_id):
         data = Hotel.arguments.parse_args()
         hotel_found = HotelModel.find_hotel(hotel_id)
@@ -52,6 +55,7 @@ class Hotel(Resource):
         except:
             return { 'message': 'An internal error ocurred trying to save data.'}, 500
 
+    @jwt_required()
     def delete(self, hotel_id):
         hotel = HotelModel.find_hotel(hotel_id)
         if hotel:
--- Basic_Hotel_API/resources/user.py
+++ Basic_Hotel_API/resources/user.py
@@ -1,6 +1,6 @@
 import hmac
 
-from flask_jwt_extended import create_access_token
+from flask_jwt_extended import create_access_token, jwt_required
 from flask_restful import Resource, reqparse
 from models.user import UserModel
 
@@ -16,6 +16,7 @@ class User(Resource):
         if user: return user.json()
         return {'message': 'User not found!'}, 404
 
+    @jwt_required()
     def delete(self, user_id):
         user = UserModel.find_user(user_id)
         if user:
