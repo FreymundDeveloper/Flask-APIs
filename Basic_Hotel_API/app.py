b0b86ad713cdc1c8e7131fd563fbf1b64a048da6
Lucas Freymund <lucasehresmann.osik@gmail.com>
2024-06-18 18:03:18 -0300

    Logout System

--- Basic_Hotel_API/app.py
+++ Basic_Hotel_API/app.py
@@ -1,23 +1,42 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
 
+from blacklist import BLACKLIST
 from resources.hotel import Hotels, Hotel
-from resources.user import User, UserReagister, UserLogin
+from resources.user import User, UserReagister, UserLogin, UserLogout
+
+## App configs
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///flaskapis.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['JWT_SECRET_KEY'] = 'ThisIsAKey'
+app.config['JWT_BLACKLIST_ENABLED'] = True
 
 api = Api(app)
 jwt = JWTManager(app)
-    
+
+## API Routes
+
 api.add_resource(Hotels, '/hotels')
 api.add_resource(Hotel, '/hotels/<string:hotel_id>')
 api.add_resource(User, '/users/<int:user_id>')
 api.add_resource(UserReagister, '/register')
 api.add_resource(UserLogin, '/login')
+api.add_resource(UserLogout, '/logout')
+
+## Token Validates
+
+@jwt.token_in_blocklist_loader
+def verify_blacklist(self,token):
+    return token['jti'] in BLACKLIST
+
+@jwt.revoked_token_loader
+def access_token_invalid(jwt_header, jwt_payload):
+    return jsonify({ 'message': 'You have been logged out!'}), 401
+
+## App running
 
 if __name__ == '__main__':
     from sql_alchemy import database
